b9fe4e5bcd14812dd0e54b52e375191d71c18293
Anil Muppalla <anil@spotify.com>
2020-04-19 20:57:05 -0400

    remove copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
-import ProTip from "./ProTip";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import { createStyles, Theme } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-
-function Copyright(): JSX.Element {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://github.com/makmade">
-        Mak Made Inc.
-      </Link>
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -42,13 +27,6 @@ export default function App(): JSX.Element {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example with TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
     </div>
   );
 }
