7f42f11aa76d9a8d3dae9e7521cca571017b1cdc
Anil Muppalla <anil@spotify.com>
2020-04-19 20:28:33 -0400

    set up dummy react app with github deploy

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import ProTip from "./ProTip";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { createStyles, Theme } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+function Copyright(): JSX.Element {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+export default function App(): JSX.Element {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Heading
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example with TypeScript
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </div>
+  );
+}
